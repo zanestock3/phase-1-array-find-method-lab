c96a7e6a6a1e5bea65d140875d9f330a5811eb27
Zane Stock <zanestock3@gmail.com>
2022-04-12 19:29:48 -0500

    commit tree 71f92d82a2e832dbb248808ac9f94a1d53175001

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // code your solution here
+function superbowlWin(year) {
+    for (const record of year) {
+        if (record.result === "W") {
+            return record.year
+        }
+    }
+}
+superbowlWin(result);
